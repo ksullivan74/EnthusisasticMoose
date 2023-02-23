9cecbb4803fd24ee7cd10f0498b5ccd3e1f6bde8
Neil Sullivan <ac30delay@gmail.com>
2023-02-23 10:55:04 -0600

    Magic Moose Works!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+
+List<string> MagicMooseAnswers = new List<string>()
+{ "As I see it, yes.", "Ask again later", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt", "Yes", "Yes - definitely", "You may rely on it" };
 
 void Main()
 {
@@ -10,11 +14,15 @@ void Main()
     MooseSays("H I, I'M  E N T H U S I A S T I C !");
     MooseSays("I really am enthusiastic");
 
+    //Magic Moose
+    MagicMoose();
+
+
     // Ask some questions
-    CanadaQuestion();
-    EnthusiasticQuestion();
-    LoveCSharpQuestion();
-    SecretQuestion();
+    //CanadaQuestion();
+    //EnthusiasticQuestion();
+    //LoveCSharpQuestion();
+    //SecretQuestion();
 
 }
 
@@ -76,6 +84,7 @@ bool MooseAsks(string question)
     }
 }
 
+/*
 void CanadaQuestion()
 {
     bool isTrue = MooseAsks("Is Canada real?");
@@ -127,5 +136,25 @@ void SecretQuestion()
         MooseSays("Oh, no...secrets are the best, I love to share them!");
     }
 }
+*/
 
+void MagicMoose()
+{
+
+    Random r = new Random();
+    r.Next(0, MagicMooseAnswers.Count);
+
+    string UserQuestion = "";
+    do
+    {
+        Console.Write("Please ask a question, after all I am a magic moose! (Hit Enter to Exit):  ");
+        UserQuestion = Console.ReadLine();
+        if (UserQuestion.Length > 0)
+        {
+            MooseSays(MagicMooseAnswers[r.Next(0, MagicMooseAnswers.Count)]);
+        }
+    }
+    while
+    (UserQuestion.Length > 0);
+}
 
